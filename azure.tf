6fcafc1dfd825207124f08d6eb8145160aca4981
Peter Bona <bonapeti@gmail.com>
2018-12-20 19:52:14 +0100

    Added frontend, backend VMs

--- azure.tf
+++ azure.tf
@@ -28,6 +28,15 @@ resource "azurerm_subnet" "backend" {
         address_prefix       = "10.0.1.0/24"
 }
 
+resource "azurerm_public_ip" "frontend_pip" {
+  name                         = "frontend_pip"
+  location                     = "${azurerm_resource_group.terraform_eval.location}"
+  resource_group_name          = "${azurerm_resource_group.terraform_eval.name}"
+  public_ip_address_allocation = "Dynamic"
+  idle_timeout_in_minutes      = 30
+
+}
+
 
 resource "azurerm_network_interface" "terraform_eval_frontend_if" {
   name                = "frontend_if"
@@ -38,6 +47,7 @@ resource "azurerm_network_interface" "terraform_eval_frontend_if" {
     name                          = "frontend_configuration"
     subnet_id                     = "${azurerm_subnet.frontend.id}"
     private_ip_address_allocation = "dynamic"
+    public_ip_address_id 	  = "${azurerm_public_ip.frontend_pip.id}"
   }
 }
 
@@ -53,3 +63,66 @@ resource "azurerm_network_interface" "terraform_eval_backend_if" {
   }
 }
 
+resource "azurerm_virtual_machine" "dwe-bonapeti-lf001" {
+	name = "dwe-bonapeti-lf0001"
+	location            = "${azurerm_resource_group.terraform_eval.location}"
+        resource_group_name = "${azurerm_resource_group.terraform_eval.name}"
+	network_interface_ids = ["${azurerm_network_interface.terraform_eval_frontend_if.id}"]
+	vm_size	= "Standard_B1s"
+
+	os_profile {
+    		computer_name  = "dwe-bonapeti-lf001"
+    		admin_username = "bonapet"
+    		admin_password = "Gikszer!"
+  	}
+
+	storage_image_reference {
+    		publisher = "OpenLogic"
+		offer = "CentOS"
+    		sku       = "7.5"
+    		version   = "latest"
+  	}
+
+	storage_os_disk {
+    		name              = "frontend_osdisk"
+    		caching           = "ReadWrite"
+    		create_option     = "FromImage"
+    		managed_disk_type = "Standard_LRS"
+  	}
+	
+	os_profile_linux_config {
+    		disable_password_authentication = false
+  	}
+}
+
+resource "azurerm_virtual_machine" "dwe-bonapeti-lb001" {
+        name = "dwe-bonapeti-lb0001"
+        location            = "${azurerm_resource_group.terraform_eval.location}"
+        resource_group_name = "${azurerm_resource_group.terraform_eval.name}"
+        network_interface_ids = ["${azurerm_network_interface.terraform_eval_backend_if.id}"]
+        vm_size = "Standard_B1s"
+
+        os_profile {
+                computer_name  = "dwe-bonapeti-lb001"
+                admin_username = "bonapet"
+                admin_password = "Gikszer!"
+        }
+
+        storage_image_reference {
+                publisher = "OpenLogic"
+                offer = "CentOS"
+                sku       = "7.5"
+                version   = "latest"
+        }
+
+        storage_os_disk {
+                name              = "backend_osdisk"
+                caching           = "ReadWrite"
+                create_option     = "FromImage"
+                managed_disk_type = "Standard_LRS"
+        }
+
+        os_profile_linux_config {
+                disable_password_authentication = false
+        }
+}
